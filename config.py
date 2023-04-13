2b74a6bbe06537ca2c7d32aafd1dbfb3283adba1
cheahkm <127661698+cheahkm@users.noreply.github.com>
2023-04-13 15:10:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cxzktwizwqpa.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "cheahkarman-employee"
+customregion = "us-east-1"
